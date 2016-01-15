b52d52077f2013ed4d918ce49465fff3c7362ff2
Marie Sgouros <ms5123@barnard.edu>
2016-01-15 11:25:08 -0500

    Changed background color by one click with javascript

--- js/index.js
+++ js/index.js
@@ -17,4 +17,10 @@ $(document).ready(function() {
 
 });
 
+window.onclick = myFunction;
+
+// If the user clicks in the window, set the background color of <body> to yellow
+function myFunction() {
+    document.getElementsByTagName("BODY")[0].style.backgroundColor = "black";
+
 });
