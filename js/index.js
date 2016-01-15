c8c6baef2b4ea50e776b81394695aa0ad500fb8a
Marie Sgouros <ms5123@barnard.edu>
2016-01-15 11:29:22 -0500

    Needed to close bracket in recent added javascript code

--- js/index.js
+++ js/index.js
@@ -19,8 +19,8 @@ $(document).ready(function() {
 
 window.onclick = myFunction;
 
-// If the user clicks in the window, set the background color of <body> to yellow
 function myFunction() {
     document.getElementsByTagName("BODY")[0].style.backgroundColor = "black";
+  }
 
 });
